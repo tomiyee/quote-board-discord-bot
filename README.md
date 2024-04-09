976f5e53b58cd0965fdfb7939e9e680c34716f32
Tommy S Heng <tommy.seng.heng@gmail.com>
2024-04-09 12:55:37 -0400

    feat: load token from env

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # quote-board-discord-bot
+
+
+Based on the quick start here.
+https://discordpy.readthedocs.io/en/stable/quickstart.html
+
+## Hardware:
+
+Developed on Ubuntu (WSL-2 specifically).
+
+## Installation
+
+To get started with the Discord Bot: 
+
+https://discordpy.readthedocs.io/en/stable/intro.html#installing
---
+++ example_bot.py
@@ -0,0 +1,23 @@
+# This example requires the 'message_content' intent.
+import os
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+print(os.environ['HOME'])
+
+client.run(os.environ["DISCORD_TOKEN"])
