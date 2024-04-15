e551e0685cd396044b79f191e3bbf5a41a53fd78
Tommy Seng Heng <tommy.seng.heng@gmail.com>
2024-04-14 23:25:00 -0400

    feat: load .env variables (#4)
    * feat: load .env variables
    
    * requirements

--- example_bot.py
+++ example_bot.py
@@ -1,6 +1,9 @@
 # This example requires the 'message_content' intent.
 import os
+from dotenv import load_dotenv
 import discord
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,13 +18,16 @@ async def on_ready() -> None:
 
 @client.event
 async def on_message(message: discord.Message) -> None:
+    # Ignore messages sent by the bot itself
     if message.author == client.user:
         return
-
+    # Respond to "$hello" with "Hello!""
     if message.content.startswith("$hello"):
         await message.channel.send("Hello!")
 
 
-print(os.environ["HOME"])
-
-client.run(os.environ["DISCORD_TOKEN"])
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+if DISCORD_TOKEN is None:
+    print("DISCORD_TOKEN is not set")
+    exit(1)
+client.run(DISCORD_TOKEN)
